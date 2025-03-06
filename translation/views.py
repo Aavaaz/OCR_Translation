b00733c40c777e0f45c65c39813a28f5c90cda68
rohitgurav <rohitgurav10142@gmail.com>
2025-03-06 10:16:45 +0530

    test

--- translation/views.py
+++ translation/views.py
@@ -23,7 +23,7 @@ client = AzureOpenAI(
 
 # Default Hello endpoint
 def hello(request):
-    return JsonResponse({'message': 'Hello'})
+    return JsonResponse({'message': 'Hello a'})
 
 # Translation endpoint
 def translate(request):
